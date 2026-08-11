1a19746666efabf367c1e559109154c946ad0653
Banana Studios <mommyfake71@gmail.com>
2026-08-10 22:20:09 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,3 +1,6 @@
+-- a mod script needs to be named "Main.lua", thats the first script you run. here you include code to run other scripts in the Directory.
+-- some games do not support certain things, make sure to test if your mod works first!
+
 -- This is required so the engine can tell what events exist in the script. do not edit it.
 Banana.Event = {}
 
